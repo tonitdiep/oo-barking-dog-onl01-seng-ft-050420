7b0ef45d838243e2d97ee366e76a58ae0ed6e929
Toni  Diep <adiep123@gmail.com>
2020-05-08 01:01:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
 
-
+# setter method .name = to give a dog name
 def name(dogs_name)
   @dogs_name = dogs_name
 end
-
+# getting method .name
 def name=(dogs_name)
   @dogs_name=dogs_name
 end
-
+#method, .bark & puts "woof!" when is called
 def bark(dogs_bark)
   @this_dogs_bark = dogs_bark
 end

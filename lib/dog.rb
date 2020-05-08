07889ce3521cf8f4e8e203bf3707b2188adac76e
Toni  Diep <adiep123@gmail.com>
2020-05-08 01:39:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,11 @@
 class Dog
 attr_accessor :name
-# # setter method .name = to give a dog name
-# def name(dogs_name)
-#   @dogs_name = dogs_name
-# end
-# # getting method .name
-# def name=(dogs_name)
-#   @dogs_name=dogs_name
-# end
-#method, .bark & puts "woof!" when is called
+
 def bark
   puts "woof!"
 end
 
 end
 
-# fido = Dog.new
-# fido.bark
+fido = Dog.new
+fido.bark

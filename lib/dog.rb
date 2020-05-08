3757a7682f55c640a8d774a9648f8007133ccf7c
Toni  Diep <adiep123@gmail.com>
2020-05-08 01:05:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,12 +10,9 @@ def name=(dogs_name)
 end
 #method, .bark & puts "woof!" when is called
 def bark(dogs_bark)
-  @this_dogs_bark = dogs_bark
+  puts "woof!"
 end
 
-def bark
-  puts "Woof!"
-end
 
 end
 
